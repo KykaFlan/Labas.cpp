2a01218472bc574399692e01c89264bcb06331e4
KykaFlan <dillerty_pro@mail.ru>
2026-02-17 18:04:34 +0500

    Update LabaC++№8.cpp

--- laba8/LabaC++№8/LabaC++№8.cpp
+++ laba8/LabaC++№8/LabaC++№8.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include "BackRec9.h"
 #include "HomeDyn3.h"
 #include "HomeDyn15.h"
@@ -7,10 +7,21 @@ int main()
 {
     using namespace std;
     setlocale(LC_ALL, "RU");
-    int Task_number;
+    int Task_number;    
     do {
-        cout << "Введите номер задания(1 - BackRec9; 2 - HomeDyn3; 3 - HomeDyn15 или 0 для выхода) : ";
+        cout << "Введите номер задания(1 - BackRec9; 2 - HomeDyn3; 3 - HomeDyn15 или 0 для выходаGG) : ";
         cin >> Task_number;
+        bool ok = false;
+        while (!ok) {
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(1000, '\n');
+                cin >> Task_number;
+            }
+            else {
+                ok = true;
+            }
+        }
         switch (Task_number) {
         case(0):
             cout << "Конец кода";
@@ -25,7 +36,10 @@ int main()
             HomeDyn15();
             break;
         default:
-            cout << "ОШИБКА\n";
+            if (!ok) {
+                cout << "ОШИБКА\n";
+            }
+            break;
         }
     } 
     while (Task_number != 0);
