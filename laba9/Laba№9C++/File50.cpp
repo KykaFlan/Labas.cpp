f6d9c909e5eb09c24cd77c6656ce721828b75dd5
KykaFlan <dillerty_pro@mail.ru>
2026-02-22 16:19:52 +0500

    Update File50.cpp

--- laba9/Laba№9C++/File50.cpp
+++ laba9/Laba№9C++/File50.cpp
@@ -8,47 +8,47 @@ void File50() {
     using namespace std;
     setlocale(LC_ALL, "RU");
     string S1, S2, S3;
-    cout << "Введите имя первого файла (S1): ";
+    cout << "Р’РІРµРґРёС‚Рµ РёРјСЏ РїРµСЂРІРѕРіРѕ С„Р°Р№Р»Р° (S1): ";
     cin >> S1;
-    cout << "Введите имя второго файла (S2): ";
+    cout << "Р’РІРµРґРёС‚Рµ РёРјСЏ РІС‚РѕСЂРѕРіРѕ С„Р°Р№Р»Р° (S2): ";
     cin >> S2;
-    cout << "Введите имя результирующего файла (S3): ";
+    cout << "Р’РІРµРґРёС‚Рµ РёРјСЏ СЂРµР·СѓР»СЊС‚РёСЂСѓСЋС‰РµРіРѕ С„Р°Р№Р»Р° (S3): ";
     cin >> S3;
 
-    // Читаем первый файл
+    // Р§РёС‚Р°РµРј РїРµСЂРІС‹Р№ С„Р°Р№Р»
     ifstream File1(S1, ios::binary);
     if (!File1) {
-        cout << "Ошибка: файл '" << S1 << "' не найден!" << endl;
+        cout << "РћС€РёР±РєР°: С„Р°Р№Р» '" << S1 << "' РЅРµ РЅР°Р№РґРµРЅ!" << endl;
         return;
     }
 
     double number1;
-    cout << "Содержимое файла " << S1 << ": ";
+    cout << "РЎРѕРґРµСЂР¶РёРјРѕРµ С„Р°Р№Р»Р° " << S1 << ": ";
     while (File1.read((char*)&number1, sizeof(double))) {
         cout << number1 << "; ";
     }
     cout << endl;
-    // Возвращаемся в начало файла
+    // Р’РѕР·РІСЂР°С‰Р°РµРјСЃСЏ РІ РЅР°С‡Р°Р»Рѕ С„Р°Р№Р»Р°
     File1.clear();
     File1.seekg(0, ios::beg);
 
-    // Читаем второй файл
+    // Р§РёС‚Р°РµРј РІС‚РѕСЂРѕР№ С„Р°Р№Р»
     ifstream File2(S2, ios::binary);
     if (!File2) {
-        cout << "Ошибка: файл '" << S2 << "' не найден!" << endl;
+        cout << "РћС€РёР±РєР°: С„Р°Р№Р» '" << S2 << "' РЅРµ РЅР°Р№РґРµРЅ!" << endl;
         return;
     }
     double number2;
-    cout << "Содержимое файла " << S2 << ": ";
+    cout << "РЎРѕРґРµСЂР¶РёРјРѕРµ С„Р°Р№Р»Р° " << S2 << ": ";
     while (File2.read((char*)&number2, sizeof(double))) {
         cout << number2 << "; ";
     }
     cout << endl;
-    // Возвращаемся в начало файла
+    // Р’РѕР·РІСЂР°С‰Р°РµРјСЃСЏ РІ РЅР°С‡Р°Р»Рѕ С„Р°Р№Р»Р°
     File2.clear();
     File2.seekg(0, ios::beg);
 
-    // Сортируем
+    // РЎРѕСЂС‚РёСЂСѓРµРј
     ofstream File3Out(S3, ios::binary);
     File1.read((char*)&number1, sizeof(double));
     File2.read((char*)&number2, sizeof(double));
@@ -75,7 +75,7 @@ void File50() {
     File3Out.close();
 
     ifstream File3C(S3, ios::binary);
-    cout << "\nРезультат в файле " << S3 << ": ";
+    cout << "\nР РµР·СѓР»СЊС‚Р°С‚ РІ С„Р°Р№Р»Рµ " << S3 << ": ";
     double i;
     while (File3C.read((char*)&i, sizeof(double))) {
         cout << i << "; ";
